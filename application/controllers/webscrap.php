b0704a7d3dd78203b1108b6d234425e5da39451a
Bigetion <adysuprianto@yahoo.co.id>
2018-11-19 07:51:56 +0700

    Project ID Webscrap

--- application/controllers/webscrap.php
+++ application/controllers/webscrap.php
@@ -23,4 +23,44 @@ class WebScrap extends Controller
 
         $this->render->json($result);
     }
+
+    public function projectId()
+    {
+        $page = 1;
+
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+
+        $this->crawl->set_url("https://projects.co.id/public/browse_projects/listing/6_website-development?page=$page");
+        $result['data'] = $this->crawl->get_data(["html as title", "href as link"], [[
+            "condition" => ["level", "endswith", "_3_1_0"],
+        ], [
+            "condition" => ["tag", "=", "a"],
+        ], [
+            "condition" => ["href", "contains", "https://projects.co.id/public/browse_projects/view/"],
+        ]]);
+
+        $description = $this->crawl->get_data([], [[
+            "condition" => ["level", "endswith", "_3_2"],
+        ], [
+            "condition" => ["tag", "=", "p"],
+        ]]);
+
+        foreach ($description as $key => $desc) {
+            $result['data'][$key]['description'] = $desc['innerHTML'];
+        }
+
+        $pages = $this->crawl->get_data([], [[
+            "condition" => ["class", "=", "ajax-url"],
+        ]]);
+
+        $result['totalPage'] = 0;
+
+        if (count($pages) > 0) {
+            $result['totalPage'] = $pages[count($pages) - 1]['paramval'];
+        }
+
+        $this->render->json($result);
+    }
 }
