8d4526bb7bbadf3a2c29ff60b80de92cd93497a3
Bigetion <adysuprianto@yahoo.co.id>
2019-05-21 18:29:51 +0700

    Fix

--- application/config/config.php
+++ application/config/config.php
@@ -1,10 +1,14 @@
 <?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+$path = str_replace('\\', '/', getcwd());
+$base_path = str_replace($root, '', $path);
 return array(
-    'base_url' => 'http://localhost/php_api_boilerplate/',
+    'base_url' => 'http://' . $_SERVER['HTTP_HOST'] . $base_path . '/',
     'default_app_method' => 'index',
     'default_project' => 'page',
     'default_project_controller' => 'home',
     'default_project_method' => 'index',
+    'secret_key' => '123456789',
     'allowed_origin' => '*',
 )
 ?>
